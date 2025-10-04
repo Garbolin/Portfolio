7ce8b8f7efd128edfc8c1ca59e98235bea4a4a29
Gabriela Buil <134524103+Garbolin@users.noreply.github.com>
2025-10-04 22:59:06 +0200

    C-869aq22p5 : sending email working succesfully

--- JS/email.js
+++ JS/email.js
@@ -4,20 +4,31 @@ document.addEventListener("DOMContentLoaded", () => {
     const inputEmail = document.querySelector("#email");
     const inputSubject = document.querySelector("#subject");
     const inputMessage = document.querySelector("#message");
-    const submit = document.querySelector("#submit");
+    const btnSubmit = document.querySelector("#submit");
+    const loader = document.querySelector("#loader");
+    const form = document.querySelector("#form");
+
+    email = {
+        name : "",
+        email : "",
+        subject : "",
+        message : "",
+    }
 
     inputName.addEventListener("blur", validate);
     inputEmail.addEventListener("blur", validate);
     inputSubject.addEventListener("blur", validate);
     inputMessage.addEventListener("blur", validate);
 
-    submit.addEventListener("click", (e) => {
+    btnSubmit.addEventListener("click", (e) => {
         e.preventDefault()
-        console.log("Enviando email...");
+        isDisabled();
+
     })
 
     function validate(e) {
         if (e.target.value.trim() === ""){
+            email[e.target.name] = "";
             //Comprobar si ya hay alerta
             removeAlert( e.target.parentElement );
             showAlert(`* hay que poner un contenido en el ${e.target.parentElement.querySelector("label").textContent}`, e.target.parentElement);
@@ -27,13 +38,14 @@ document.addEventListener("DOMContentLoaded", () => {
         if (e.target.id === "email" && e.target.value.trim() !== "") {
             //Validar email
             if (!validateEmail(e.target.value.trim())){
+                email[e.target.name] = "";
                 showAlert(`* Creo que el email no está bien construido :P`, e.target.parentElement);
                 return;
             }
         }
-
         //han pasado la prueba
         removeAlert(e.target.parentElement)
+        email[e.target.name] = e.target.value.trim().toLowerCase();
     }
 
     function showAlert( message, reference ) {
@@ -52,11 +64,48 @@ document.addEventListener("DOMContentLoaded", () => {
 
     //Comprobar si ya hay alerta, si hay se borra
     function removeAlert(reference) {
-        console.log(reference)
-        if(reference.lastElementChild?.classList.contains("error-email")){
+        if(reference.lastElementChild?.classList?.contains("error-email")){
             const error = reference.lastElementChild;
             reference.removeChild(error);
         }
     }
+
+    function refreshEmail() {
+        
+        Object.keys(email).forEach((key) => {
+            email[key] = "";
+        })
+
+        form.reset();
+    }
+
+    function isDisabled() {
+        if (Object.values(email).includes("")) {
+            btnSubmit.desabled = true;
+            return;
+        } else {
+            btnSubmit.desabled = false;
+            sendEmail();
+            refreshEmail();
+        }
+        
+    }
+    //Script file
+    async function sendEmail() {
+        try {
+        loader.classList.remove("hidden-loader"); // mostramos loader antes de enviar
+
+        const response = await emailjs.send(
+            "service_uk15g8a",
+            "template_19qjilk",
+            email
+        );
+    } catch (error) {
+        console.log('FAILED...', error);
+    } finally {
+        loader.classList.add("hidden-loader"); // siempre se oculta el loader al terminar
+    }
+    }
+
 
 })
